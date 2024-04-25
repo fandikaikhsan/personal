6613316d8b1c770ea0b9af7f01836c844226f4d5
Fandika <fandika.ikhsan@gmail.com>
2024-04-25 18:36:09 +0700

    feat: project card carousel wrapper

---
+++ src/components/wraps/ProjectCarousel.tsx
@@ -0,0 +1,24 @@
+import ProjectCard from "@/components/cards/ProjectCard"
+
+type Project = {
+  image: string
+  title: string
+  short_desc: string
+}
+
+export default function ProjectCarousel({ projects }: { projects: Project[] }) {
+  return (
+    <div className="flex flex-col w-full gap-5 justify-center text-center p-5">
+      <div className="flex flex-row gap-5 overflow-scroll bg-blue-700 justify-start text-center">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            image={project.image}
+            title={project.title}
+            short_desc={project.short_desc}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
