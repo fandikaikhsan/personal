767d24e716476aa7542845fa13b5111065dac327
Fandika <fandika.ikhsan@gmail.com>
2024-04-25 19:55:34 +0700

    chore: change image project size

--- src/constants/HomeConstant.ts
+++ src/constants/HomeConstant.ts
@@ -1,41 +1,41 @@
 const ProjectsConstant = [
   {
-    image: "https://via.placeholder.com/150x180",
+    image: "https://via.placeholder.com/300x180",
     title: "Project 1",
     short_desc: "Short description of project 1",
   },
   {
-    image: "https://via.placeholder.com/150x180",
+    image: "https://via.placeholder.com/300x180",
     title: "Project 2",
     short_desc: "Short description of project 2",
   },
   {
-    image: "https://via.placeholder.com/150x180",
+    image: "https://via.placeholder.com/300x180",
     title: "Project 3",
     short_desc: "Short description of project 3",
   },
   {
-    image: "https://via.placeholder.com/150x180",
+    image: "https://via.placeholder.com/300x180",
     title: "Project 4",
     short_desc: "Short description of project 4",
   },
   {
-    image: "https://via.placeholder.com/150x180",
+    image: "https://via.placeholder.com/300x180",
     title: "Project 5",
     short_desc: "Short description of project 5",
   },
   {
-    image: "https://via.placeholder.com/150x180",
+    image: "https://via.placeholder.com/300x180",
     title: "Project 6",
     short_desc: "Short description of project 6",
   },
   {
-    image: "https://via.placeholder.com/150x180",
+    image: "https://via.placeholder.com/300x180",
     title: "Project 7",
     short_desc: "Short description of project 7",
   },
   {
-    image: "https://via.placeholder.com/150x180",
+    image: "https://via.placeholder.com/300x180",
     title: "Project 8",
     short_desc: "Short description of project 8",
   },
