59a208d7f30164bfeed4b60b6bf916a165eabdaa
Fandika <fandika.ikhsan@gmail.com>
2024-04-25 19:20:19 +0700

    feat: add homepage name intro wrapper

---
+++ src/components/wraps/HomeIntroWrap.tsx
@@ -0,0 +1,16 @@
+export default function HomeIntroWrap() {
+  return (
+    <div className="flex flex-col w-[80%] md:w-[50%] lg:w-[40%] text-end px-10 gap-5 mt-10">
+      <h1 className="text-[3rem] lg:text-[6rem] font-thin leading-[4rem] lg:leading-[6rem] uppercase">
+        Fandika Ikhsan
+      </h1>
+      <h2 className="w-full text-sm font-thin">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate vel
+        est accusantium sint doloremque similique magni pariatur praesentium
+        commodi debitis? Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Voluptate vel est accusantium sint doloremque similique magni
+        pariatur praesentium commodi debitis?
+      </h2>
+    </div>
+  )
+}
