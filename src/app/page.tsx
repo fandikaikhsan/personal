0f4d7c94e7e76dbb4533293b2dd3e0d003a3034e
Fandika <fandika.ikhsan@gmail.com>
2024-04-25 19:20:53 +0700

    chore: embed latest progress to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import ProjectCard from "@/components/cards/ProjectCard"
 import { ProjectsConstant } from "@/constants/HomeConstant"
 import ProjectCarousel from "@/components/wraps/ProjectCarousel"
+import HomeIntroWrap from "@/components/wraps/HomeIntroWrap"
 
 export default function Home() {
   return (
-    // <main>
-    <div className="w-full">
-      <ProjectCarousel projects={ProjectsConstant} />
-    </div>
-    // </main>
+    <main className="flex flex-col">
+      <div className="w-full">
+        <ProjectCarousel projects={ProjectsConstant} />
+      </div>
+      <HomeIntroWrap />
+      <div className="h-[70rem]">""</div>
+    </main>
   )
 }
